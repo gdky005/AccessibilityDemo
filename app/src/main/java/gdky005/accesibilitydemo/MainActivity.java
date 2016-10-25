20ab1b36a5325eda70c9cb2ea899fdbf8de13951
wangqing <wangqing@kaolafm.com>
2016-10-25 17:55:36 +0800

    1. 初始化辅助功能。

---
+++ app/src/main/java/gdky005/accesibilitydemo/MainActivity.java
@@ -0,0 +1,35 @@
+package gdky005.accesibilitydemo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Toast.makeText(this, "请优先开启权限", Toast.LENGTH_SHORT).show();
+
+//        startService(new Intent(this, WQAccessibilityService.class));
+
+    }
+
+}
